4fc08d76f6441b2471e130075d5edcfb98148dc6
Oscar GUERIN <oscar.guerin@gmail.com>
2020-08-05 19:55:08 +0200

    feat(action): edit action component

---
+++ src/app/@shared/edit-action/edit-action.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-edit-action',
+	templateUrl: './edit-action.component.html'
+})
+export class EditActionComponent {
+	public constructor() {
+	}
+}
