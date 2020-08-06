e5cb23579753a5bfb545e126ec07be12ea427d71
Oscar GUERIN <oscar.guerin@gmail.com>
2020-08-06 22:53:08 +0200

    feat(action): dialog ref close

--- src/app/@shared/edit-action/edit-action.component.ts
+++ src/app/@shared/edit-action/edit-action.component.ts
@@ -6,7 +6,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { first } from 'rxjs/operators';
 import { ActionService } from '../../@core/services/action.service';
 import { Action } from '../../@core/models/action';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Crop } from '../../@core/models/crop';
 
 interface EditActionDialogData {
@@ -23,6 +23,7 @@ export class EditActionComponent extends ObservableDestroy {
 	public form: FormGroup;
 
 	public constructor(@Inject(MAT_DIALOG_DATA) public data: EditActionDialogData,
+					   private readonly dialogRef: MatDialogRef<EditActionComponent>,
 					   private readonly route: ActivatedRoute,
 					   private readonly router: Router,
 					   private readonly actionService: ActionService,
@@ -43,7 +44,7 @@ export class EditActionComponent extends ObservableDestroy {
 	public submit(): void {
 		this.actionService.createOrUpdate(this.data.action.merge(this.form.value, { cropId: this.data.crop.id })).pipe(
 			first()
-		).subscribe(() => this.router.navigateByUrl('/'));
+		).subscribe(() => this.dialogRef.close());
 	}
 
 	public addStep(stepValue?: string): void {
