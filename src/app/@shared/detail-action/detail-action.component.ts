5b76eec39ee624552b1dd21d6b3a52b47484ff28
Oscar GUERIN <oscar.guerin@gmail.com>
2020-09-01 11:13:04 +0200

    feat(action): layout action accordion

--- src/app/@shared/detail-action/detail-action.component.ts
+++ src/app/@shared/detail-action/detail-action.component.ts
@@ -3,7 +3,12 @@ import { Action } from '../../@core/models/action';
 
 @Component({
 	selector: 'app-detail-action',
-	templateUrl: './detail-action.component.html'
+	templateUrl: './detail-action.component.html',
+	styles: [`
+        :host {
+            width: 100%;
+        }
+	`]
 })
 export class DetailActionComponent {
 	@Input()
