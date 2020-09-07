eebfec09697664b1fc672041366c4734677cca5c
Oscar GUERIN <oscar.guerin@gmail.com>
2020-09-07 18:41:18 +0200

    feature(action): edit action button

--- src/app/@shared/detail-action/detail-action.component.ts
+++ src/app/@shared/detail-action/detail-action.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Action } from '../../@core/models/action';
 import { ActionService } from '../../@core/services/action.service';
 
@@ -14,8 +14,14 @@ import { ActionService } from '../../@core/services/action.service';
 export class DetailActionComponent {
 	@Input()
 	public action: Action;
+	@Output()
+	public edit$: EventEmitter<void> = new EventEmitter<void>();
 
 	public constructor(private readonly actionService: ActionService) {
+	}
+
+	public edit(): void {
+		this.edit$.emit();
 	}
 
 	public delete(): void {
