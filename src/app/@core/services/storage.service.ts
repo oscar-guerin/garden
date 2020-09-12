889a1a3a6fd6868fcf28751c710716d1520bb66b
Oscar GUERIN <oscar.guerin@gmail.com>
2020-09-12 19:31:10 +0200

    build(storage): storage service

---
+++ src/app/@core/services/storage.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
+import { map } from 'rxjs/operators';
+import { merge, Observable } from 'rxjs';
+
+export interface UploadStatus {
+	progress?: number;
+	downloadUrl?: any;
+}
+
+@Injectable()
+export class StorageService {
+
+	public constructor(private readonly storage: AngularFireStorage) {
+	}
+
+	public upload(file: any, path: string): Observable<UploadStatus> {
+		const fileRef: AngularFireStorageReference = this.storage.ref(path);
+		const task: AngularFireUploadTask = this.storage.upload(path, file);
+
+		const percentageStatus$: Observable<UploadStatus> = task.percentageChanges().pipe(
+			map((percentage: number) => ({ progress: percentage }))
+		);
+
+		const downloadUrlStatus$: Observable<UploadStatus> = fileRef.getDownloadURL().pipe(
+			map((url: any) => ({ downloadUrl: url }))
+		);
+
+		return merge(percentageStatus$, downloadUrlStatus$);
+	}
+}
