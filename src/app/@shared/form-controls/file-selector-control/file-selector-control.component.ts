157ea4ab527db20c3abcab82002f1a5c10a58125
Oscar GUERIN <oscar.guerin@gmail.com>
2020-09-13 14:44:35 +0200

    feat(crop): select file

--- src/app/@shared/form-controls/file-selector-control/file-selector-control.component.ts
+++ src/app/@shared/form-controls/file-selector-control/file-selector-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { AbstractPrimitiveControl } from '../abstract-primitive-control';
 
@@ -15,7 +15,27 @@ import { AbstractPrimitiveControl } from '../abstract-primitive-control';
 })
 export class FileSelectorControlComponent extends AbstractPrimitiveControl<string> {
 
+	@Input()
+	public maxLength: number = 5242880;
+	@Input()
+	public imagesOnly: boolean = false;
+
+	public selectedFile: File;
+
 	public constructor() {
 		super('file', '');
+	}
+
+	public onFileSelected(event: any): void {
+		if (this.imagesOnly && !event.target.files[0].type.startsWith('image')) {
+			// TODO toaster
+			return;
+		}
+		if (event.target.files[0].size > this.maxLength) {
+			// TODO toaster
+			return;
+		}
+
+		this.selectedFile = event.target.files[0];
 	}
 }
