5748229aedfaeb12435d43f5a59a57ec72012740
Oscar GUERIN <oscar.guerin@gmail.com>
2020-09-23 10:27:51 +0200

    feature(about): about routing and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeModule } from './home/home.module';
 import { CropsModule } from './crops/crops.module';
+import { AboutModule } from './about/about.module';
 
 const routes: Routes = [
 	{
@@ -11,6 +12,10 @@ const routes: Routes = [
 	{
 		path: 'crops',
 		loadChildren: () => import('./crops/crops.module').then((m: { CropsModule: CropsModule }) => m.CropsModule)
+	},
+	{
+		path: 'about',
+		loadChildren: () => import('./about/about.module').then((m: { AboutModule: AboutModule }) => m.AboutModule)
 	},
 	{
 		path: '',
